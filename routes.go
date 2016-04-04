d63207d93c3df3a4c7148a85ec9262c46fb166cc
Jason Clark <jason@jjcconsultingllc.com>
2016-04-03 17:38:39 -0700

    Start API routes with default going to not found

--- routes.go
+++ routes.go
@@ -6,6 +6,20 @@ func createRouter() http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/app", appHandler())
 	mux.Handle("/", rootHandler())
+	mux.Handle("/api/", http.StripPrefix("/api", apiRoutes()))
 
 	return middlewareStack(mux)
+}
+
+func apiRoutes() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", notFoundHandlerFunc)
+	return mux
+}
+
+func notFoundHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	err := handleAsTemplateFile(w, http.StatusNotFound, "templates/404.html", struct{ Title string }{AppName})
+	if err != nil {
+		http.Error(w, "Template error", http.StatusNotFound)
+	}
 }
